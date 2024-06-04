5bbeb056f62c2ce8683c6ec6c6f43f0d31977676
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-06-04 14:48:07 +0000

    Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 912899
+let accountEmail = "nikh@gmail.com"
+var accountPassword = "1233"
+accountCity = "Bgp"
+ let accountState;
+
+// accountId = 957214
+accountEmail= "nikhil@gmail.com"
+accountPassword="121212"
+accountCity= "Mfp"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3+3) we are using nodejs , not browser
+
+console.log(3+3)
+
+console.log("Nikhil")
+
+let name = "Nikhil"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value  --  null is a object
+//undefined => 
+// symbol => uinque
+
+//object 
+
+console.log(typeof age);
