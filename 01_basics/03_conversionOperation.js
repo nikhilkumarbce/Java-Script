85bf5a5964ffb999f2ff0176efb3310ecfb91649
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-06-05 03:56:02 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score)) //Can be written like this also.
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+
+let isLoggedIN = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+// console.log(booleanIsLoggedIn)
+ 
+
+// 1 => true; 0 => false
+//" " => false
+//"Nikhil" => true
+
+let someNumber = 55
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber)
+console.log(stringSomeNumber)
