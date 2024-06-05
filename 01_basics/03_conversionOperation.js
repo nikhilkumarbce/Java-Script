349427caf2ab9bb35350dfe070669f1e25e09fd6
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-06-05 06:09:23 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIN)
 
 let someNumber = 55
 let stringSomeNumber = String(someNumber)
-console.log(typeof stringSomeNumber)
-console.log(stringSomeNumber)
+// console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber)
+
+
+
+//  *****************     OPERATIONS    ******************//
+
+
+let value = 3 
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Nikhil"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4 ) * 5 % 3 );
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+// console.table([num1, num2, num3]);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
