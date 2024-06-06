fff175b6b4d7d6b5874e652445a049ee641a1fca
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-06-06 05:53:00 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Nikhil"
+const repoCount = 10
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Nikhil-kumar-profile")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('h'))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(2,4)
+// console.log(anotherString);
+
+// const newStringOne = "   Nikhil   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https:/nikhil.com/nikhhil%20kumar"
+// console.log(url.replace('%20','+'))
+
+console.log(url.includes('happy'))
+
+
+console.log(gameName.split('-'))
+
