2c7082e3caf00a10fa803e3738d2477a21a0151b
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-07-01 04:24:46 +0000

    Control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,27 @@
+// if
+
+const temperature = 43
+
+// if (temperature <=  41) {
+//     console.log("Temperature is less tha 42")
+// }
+// else{
+//     console.log("Temperature is greater than 42")
+// }
+// console.log("Executed")
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`)   
+// }
+
+// console.log(`User power: ${power}`)
+
+
+// const balance = 2000
+// if (balance > 1000) console.log("test");
+
