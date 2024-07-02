d5847ffd1ce038cb47529cb87b78c17c4f06c3d7
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-07-02 04:21:00 +0000

    nested

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -25,3 +25,29 @@ const temperature = 43
 // const balance = 2000
 // if (balance > 1000) console.log("test");
 
+// if (balance < 500) {
+//     console.log("less than 500")
+// }
+//  else if (balance < 750) {
+//     console.log("less than 750")
+// }
+// else if (balance < 900) {
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 2100")
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy courses")
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User logged in")
+}
