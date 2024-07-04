ab06dec3264e3250ad5c9e94b988dc4efc7c741a
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-07-04 05:03:48 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+let index = 0
+while (index <= 10) {
+    // console.log(index);
+    index = index + 2;
+}
+
+let myArr = ["batman", "superman", "flash"]
+
+let i = 0
+while (i < myArr.length) {
+    // console.log(`Value is ${myArr[i]}`);
+    i = i + 1
+}
+
+let score = 1
+ do {
+    console.log(`Score is ${score}`)
+    score++
+ } while (score <= 10);
