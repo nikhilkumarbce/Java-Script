ef43fda065ae32f88fdb34d78465da2d1dba4523
nikhilkumarbce <161204627+nikhilkumarbce@users.noreply.github.com>
2024-07-05 04:33:00 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,35 @@
+
+const numbers = [1,2,3,4,5]
+
+for (const num of numbers) {
+    // console.log(num);
+}
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('UK', "United Kingdom")
+map.set('FR', "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ":-", value);
+}
+
+const myObject = {
+    game1 : "NFS",
+    game2 : "Spiderman"
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, value);
+
+// }
